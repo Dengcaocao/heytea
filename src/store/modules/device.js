6f14a626c4aceaf41ec29bbee6ad6961f53f96cd
dengwei <1536698087@qq.com>
2021-06-25 16:11:16 +0800

    device 模块store

---
+++ src/store/modules/device.js
@@ -0,0 +1,17 @@
+export default {
+  // namespaced: true,
+  state: {
+    device: {
+      system: '', // Ios、Android
+      channel: '', // 打开h5的系统与浏览器。微信浏览器、qq浏览器、抖音浏览器、微博浏览器、支付宝浏览器
+      platform: '', // 平台,默认web
+    }
+  },
+  getters: {},
+  mutations: {
+    setDevice(state, data) {
+      state.device = data
+    }
+  },
+  actions: {},
+}
