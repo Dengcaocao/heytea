07d73f24913eff312cf0d6446d952aa3e9a496d4
邓威 <1536698087@qq.com>
2021-06-27 22:26:38 +0800

    解决page设置100%时tabBar遮挡内容的问题

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,11 @@
 <style lang="scss">
 	/*每个页面公共css */
 	@import './assets/scss/common.scss';
+	page{
+		/**解决page设置100%时，tabBar遮挡内容的问题 */
+		height: auto;
+    min-height: 100%;
+		font-size: 28rpx;
+		background-color: #F1F8FA;
+	}
 </style>
