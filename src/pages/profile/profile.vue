2a65eb00b5364eb1136bfd176f1c76daa9480c58
dengwei <1536698087@qq.com>
2021-06-28 17:19:10 +0800

    profile页面静态布局

--- src/pages/profile/profile.vue
+++ src/pages/profile/profile.vue
@@ -1,5 +1,236 @@
 <template>
 	<view class="profile">
-		profile
+		<!-- bg -->
+		<view class="bg">
+			<image src="https://img-shop.qmimg.cn/s16/images/2020/01/20/9a82219bedcae5c2.jpeg"></image>
+			<button type="default" size="mini" class="member-code flex">
+				<image class="code" src="@/assets/images/profile/hym.png"></image>
+				<text class="txt">会员码</text>
+			</button>
+		</view>
+		<!-- content -->
+		<view class="content">
+			<view class="info-warp">
+				<view class="user-info flex">
+					<view class="avatar">
+						<image src="@/assets/images/profile/default.png" mode="widthFix"></image>
+					</view>
+					<view class="warp">
+						<view class="name">请点击授权登录</view>
+						<view class="growth-value">当前成长值0/0</view>
+						<view class="pregress"></view>
+					</view>
+					<view class="privilege flex-align">
+						<text>会员权益</text>
+						<text class="iconfont icon-jiantou"></text>
+					</view>
+				</view>
+				<view class="account flex">
+					<view class="account-item">
+						<view class="count">***</view>
+						<view class="name">奈雪券</view>
+					</view>
+					<view class="account-item">
+						<view class="count">***</view>
+						<view class="name">积分商城</view>
+					</view>
+					<view class="account-item">
+						<view class="count">***</view>
+						<view class="name">余额</view>
+					</view>
+					<view class="account-item">
+						<view class="count">***</view>
+						<view class="name">礼品卡</view>
+					</view>
+				</view>
+			</view>
+			<!-- 新会员 -->
+			<view class="new-member">
+				<view class="tips flex-align flex-between">
+					<text>新用户加入会员，享会员权益</text>
+					<text>立即加入</text>
+				</view>
+				<view class="category flex">
+					<view class="category-item" v-for="(item, index) in newMemberCategory" :key="index">
+						<!-- <view class="imgIcon"> -->
+							<image class="imgIcon" :src="item.imgUrl"></image>
+						<!-- </view> -->
+						<view class="name">{{item.name}}</view>
+					</view>
+				</view>
+			</view>
+			<!-- ad -->
+			<view class="ad">
+				<image src="@/assets/images/profile/banner.png"></image>
+			</view>
+		</view>
 	</view>
 </template>
+<script>
+export default {
+	data () {
+		return {
+			newMemberCategory: [
+				{
+					imgUrl: require('@/assets/images/profile/rhyl.png'),
+					name: '入会有礼'
+				},
+				{
+					imgUrl: require('@/assets/images/profile/jfdh.png'),
+					name: '积分兑换'
+				},
+				{
+					imgUrl: require('@/assets/images/profile/sjtq.png'),
+					name: '升级特权'
+				},
+				{
+					imgUrl: require('@/assets/images/profile/srtq.png'),
+					name: '生日特权'
+				},
+				{
+					imgUrl: require('@/assets/images/profile/nxbz.png'),
+					name: '奈雪宝藏'
+				}
+			]
+		}
+	}
+}
+</script>
+<style scoped lang="scss">
+.bg{
+	position: relative;
+	image{
+		width: 100%;
+		height: 516rpx;
+		display: block;
+	}
+	.member-code{
+		position: absolute;
+		top: 40rpx;
+		right: 40rpx;
+		border-radius: 100rpx;
+		color: $themeColor;
+		font-size: 24rpx;
+		background-color: #f8f8f8;
+		&::after{
+			border: none;
+		}
+		.code{
+			width: 30rpx;
+			height: 30rpx;
+			margin-right: 10rpx;
+		}
+	}
+}
+.content{
+	padding: 0 30rpx;
+}
+.info-warp{
+	position: relative;
+	z-index: 99;
+	margin-top: -114rpx;
+	border-radius: $border-radius;
+	background-color: #FFFFFF;
+	box-shadow: $box-shadow;
+	margin-bottom: $margin-bottom;
+	.user-info{
+		.avatar{
+			width: 160rpx;
+			height: 160rpx;
+			padding: 10rpx;
+			box-sizing: border-box;
+			border-radius: 50%;
+			margin: -34rpx 34rpx 0 34rpx;
+			box-shadow: 0 0 20rpx rgba(39, 37, 37, 0.2);
+			background-color: #FFFFFF;
+			image{
+				width: 100%;
+				border-radius: inherit;
+			}
+		}
+		.warp{
+			flex: 1;
+			margin-top: 20rpx;
+			.name{
+				font-weight: 700;
+				font-size: 32rpx;
+			}
+			.growth-value{
+				font-size: 24rpx;
+				color: $themeTextColor;
+			}
+			.pregress{
+				width: 100%;
+				height: 12rpx;
+				background-color: rgb(235, 235, 235);
+			}
+		}
+		.privilege{
+			color: #FFFFFF;
+			font-size: 24rpx;
+			margin-left: 34rpx;
+    	padding: 10rpx 0 10rpx 30rpx;
+			box-sizing: border-box;
+			border-radius: 100rpx 0 0 100rpx;
+			background-color: #FAB714;
+		}
+	}
+	.account{
+		.account-item{
+			flex: 1;
+			text-align: center;
+			padding: 30rpx;
+			.count{
+				font-weight: 700;
+				color: $themeTitleColor;
+				margin-bottom: 20rpx;
+				font-size: 40rpx;
+			}
+			.name{
+				font-size: 24rpx;
+				color: $themeTextColor;
+			}
+		}
+	}
+}
+.new-member{
+	padding: 30rpx;
+	margin-bottom: $margin-bottom;
+	border-radius: $border-radius;
+	background-color: #FFFFFF;
+	box-shadow: $box-shadow;
+	.tips{
+		text:nth-child(1){
+			color: $themeTitleColor;
+		}
+		text:nth-child(2){
+			color: $themeColor;
+		}
+	}
+	.category{
+		padding: 30rpx 0 20rpx;
+		.category-item{
+			flex: 1;
+			text-align: center;
+			.imgIcon{
+				width: 80rpx;
+				height: 80rpx;
+				margin-bottom: 10rpx;
+			}
+			.name{
+				font-size: 24rpx;
+				color: $themeTextColor;
+			}
+		}
+	}
+}
+.ad{
+	height: 160rpx;
+	margin-bottom: $margin-bottom;
+	image{
+		width: 100%;
+		height: 100%;
+		border-radius: $border-radius;
+	}
+}
+</style>
