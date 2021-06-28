d71387687c513b7f5da5d8631707b8aeecc88412
dengwei <1536698087@qq.com>
2021-06-28 11:15:09 +0800

    设置getters，获取微信签名

--- src/store/modules/device.js
+++ src/store/modules/device.js
@@ -1,4 +1,5 @@
 import { getDevice } from '@/utils/utils'
+import { wxConfig } from '@/utils/wx-config'
 
 export default {
   namespaced: true,
@@ -9,7 +10,21 @@ export default {
       platform: '', // 平台,默认web
     }
   },
-  getters: {},
+  getters: {
+    device: state => state.device,
+    // system
+    system: state => state.device.system,
+    channel: state => state.device.channel,
+    // 浏览器
+    isWeixin: state => state.device.channel === 'WEIXIN',
+    isQQ: state => state.device.channel === 'QQ',
+    isDouyin: state => state.device.channel === 'DOUYIN',
+    isWeibo: state => state.device.channel === 'WEIBO',
+    isAlipay: state => state.device.channel === 'ALIPAY',
+    // 小程序
+    isWeixinMP: state => state.device.channel === 'WEIXINMP',
+    isAlipayMP: state => state.device.channel === 'ALIPAYMP'
+  },
   mutations: {
     setDevice(state, data) {
       state.device = data
@@ -18,6 +33,15 @@ export default {
   actions: {
     getDeviceInfo ({ commit }) {
       commit('setDevice', getDevice())
+    },
+    // 设置微信签名信息
+    wxJssdkInit ({}) {
+      // #ifdef
+      /**
+       * TODO 调用接口获取签名信息，将签名信息传给wxConfig
+       * wxConfig(data)
+       */
+      // #endif
     }
   },
 }
