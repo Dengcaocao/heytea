cb6bdadddd1fc43d822d323c806588e599977c0c
dengwei <1536698087@qq.com>
2021-06-28 11:14:26 +0800

    初始化jssdk设置分享内容

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store'
+import { wxShare } from '@/utils/wx-config'
 Vue.prototype.$store = store
 
 Vue.config.productionTip = false
@@ -10,6 +11,21 @@ App.mpType = 'app'
 // 初始化设备信息
 store.dispatch('device/getDeviceInfo')
 console.log(store.state.device.device.platform)
+
+// 初始化微信jssdk
+console.log(store.getters['device/isWeixin'])
+if (store.getters['device/isWeixin']) {
+  store.dispatch('device/wxJssdkInit').then(() => {
+    console.log('dispatch返回的是promiss对象')
+    const shareData = {
+      title: '', // 分享标题
+      desc: '', // 分享描述
+      link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+      imgUrl: '', // 分享图标
+    }
+    wxShare(shareData)
+  })
+}
 
 const app = new Vue({
   ...App,
