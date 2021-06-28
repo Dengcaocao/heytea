05409832eaf378e676ccac6bf55de5c33272790e
dengwei <1536698087@qq.com>
2021-06-28 14:10:41 +0800

    商品列表静态布局

--- src/pages/order/order.vue
+++ src/pages/order/order.vue
@@ -38,6 +38,31 @@
 			<!-- 商品列表 -->
 			<view class="goods-list">
 				<scroll-view class="scroll-goods" :scroll-y="true">
+					<view class="warp">
+						<!-- swiper -->
+						<swiper class="swiper" :indicator-dots="true" :autoplay="true">
+							<swiper-item v-for="(item, index) in swiperData" :key="index">
+								<image :src="item"></image>
+							</swiper-item>
+						</swiper>
+						<!-- goods -->
+						<view class="goods-warp">
+							<view class="goods-category" v-for="(category, categoryIndex) in goodsList" :key="categoryIndex">
+								<view class="goods-category-title">{{category.category}}</view>
+								<view class="goods-item flex" v-for="(good, goodIndex) in category.goods" :key="goodIndex">
+									<image class="goods-img" :src="good.goodsImg"></image>
+									<view class="goods-info flex-content flex-c">
+										<view class="goods-title">{{good.title}}</view>
+										<view class="goods-desc">{{good.desc}}</view>
+										<view class="goods-price flex flex-between">
+											<text>¥{{good.price}}</text>
+											<text class="iconfont">+</text>
+										</view>
+									</view>
+								</view>
+							</view>
+						</view>
+					</view>
 				</scroll-view>
 			</view>
 		</view>
@@ -70,7 +95,56 @@ export default {
 				'热饮推荐',
 				'美好甜品'
 			],
-			currentMenu: 0
+			currentMenu: 0,
+			swiperData: [
+				'https://img-shop.qmimg.cn/s23107/2020/04/27/4ebdb582a5185358c4.jpg?imageView2/2/w/600/h/600',
+				'https://images.qmai.cn/s23107/2020/05/08/c25de6ef72d2890630.png?imageView2/2/w/600/h/600',
+				'https://img-shop.qmimg.cn/s23107/2020/04/10/add546c1b1561f880d.jpg?imageView2/2/w/600/h/600',
+				'https://images.qmai.cn/s23107/2020/04/30/b3af19e0de8ed42f61.jpg?imageView2/2/w/600/h/600',
+				'https://img-shop.qmimg.cn/s23107/2020/04/17/8aeb78516d63864420.jpg?imageView2/2/w/600/h/600'
+			],
+			goodsList: [
+				{
+					category: '奈雪早餐',
+					goods: [
+						{
+							goodsImg: 'https://img-shop.qmimg.cn/s23107/2020/04/19/fda6dd99c83af02353.jpg?imageView2/2/w/400/h/400',
+							title: '奈雪早餐',
+							desc: '购买三明治,享早餐指定饮品半价',
+							price: 18.5
+						}
+					]
+				},
+				{
+					category: '奈雪早餐',
+					goods: [
+						{
+							goodsImg: 'https://img-shop.qmimg.cn/s23107/2020/04/19/fda6dd99c83af02353.jpg?imageView2/2/w/400/h/400',
+							title: '奈雪早餐',
+							desc: '购买三明治,享早餐指定饮品半价',
+							price: 18.5
+						},
+						{
+							goodsImg: 'https://img-shop.qmimg.cn/s23107/2020/04/19/fda6dd99c83af02353.jpg?imageView2/2/w/400/h/400',
+							title: '奈雪早餐',
+							desc: '购买三明治,享早餐指定饮品半价',
+							price: 18.5
+						},
+						{
+							goodsImg: 'https://img-shop.qmimg.cn/s23107/2020/04/19/fda6dd99c83af02353.jpg?imageView2/2/w/400/h/400',
+							title: '奈雪早餐',
+							desc: '购买三明治,享早餐指定饮品半价',
+							price: 18.5
+						},
+						{
+							goodsImg: 'https://img-shop.qmimg.cn/s23107/2020/04/19/fda6dd99c83af02353.jpg?imageView2/2/w/400/h/400',
+							title: '奈雪早餐',
+							desc: '购买三明治,享早餐指定饮品半价',
+							price: 18.5
+						}
+					]
+				}
+			]
 		}
 	},
 	onReady () {
@@ -172,6 +246,55 @@ export default {
 	.goods-list{
 		flex: 6;
 		background-color: #FFFFFF;
+		.scroll-goods{
+			.warp{
+				box-sizing: border-box;
+				padding: 20rpx;
+				.swiper{
+					width: 100%;
+					height: 306rpx;
+					image{
+						width: 100%;
+						height: 100%;
+						border-radius: 8rpx;
+					}
+				}
+			}
+		}
+	}
+}
+.goods-category{
+	.goods-category-title{
+		color: $themeTitleColor;
+		padding: 30rpx 0;
+	}
+	.goods-item{
+		margin-bottom: 30rpx;
+		.goods-img{
+			width: 160rpx;
+			height: 160rpx;
+			margin-right: 20rpx;
+			border-radius: 8rpx;
+		}
+		.goods-info{
+			flex: 1;
+			padding-right: 14rpx;
+			height: 160rpx;
+			.goods-title{
+				margin-bottom: 10rpx;
+			}
+			.goods-desc{
+				color: $themeTextColor;
+				font-size: 24rpx;
+				margin-bottom: 10rpx;
+				text-overflow: ellipsis;
+				overflow: hidden;
+				white-space: nowrap;
+			}
+			.goods-price{
+				font-weight: 600;
+			}
+		}
 	}
 }
 </style>
