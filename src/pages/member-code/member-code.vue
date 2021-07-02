3a3c9da1ab4ac153e3ab3a2c4269b98e587a74a2
dengwei <1536698087@qq.com>
2021-07-02 16:46:15 +0800

    会员码页面

---
+++ src/pages/member-code/member-code.vue
@@ -0,0 +1,74 @@
+<template>
+  <view class="member-code">
+    <view class="info">
+      <view class="user flex flex-c">
+        <view class="avatar">
+          <image src="@/assets/images/profile/default.png" mode="widthFix"></image>
+          <view class="member-tag flex">
+            <image src="@/assets/images/profile/level.png" mode="widthFix"></image>
+            <view class="level">2</view>
+          </view>
+        </view>
+        <view class="name">xww</view>
+      </view>
+      <Account :border="true" />
+    </view>
+    <view class="ad"></view>
+  </view>
+</template>
+<script>
+import Account from '@/components/account.vue'
+export default {
+  components: {
+		Account
+	}
+}
+</script>
+<style scoped lang="scss">
+.member-code{
+  padding: 20rpx 30rpx;
+}
+.info{
+  background-color: #FFFFFF;
+  border-radius: $border-radius;
+  box-shadow: $box-shadow2;
+  .user{
+    margin-top: 75rpx;
+		.avatar{
+      position: relative;
+			width: 150rpx;
+			height: 150rpx;
+			padding: 10rpx;
+			box-sizing: border-box;
+			border-radius: 50%;
+			margin-top: -75rpx;
+			box-shadow: $box-shadow2;
+			background-color: #FFFFFF;
+			image{
+				width: 100%;
+				border-radius: inherit;
+			}
+      .member-tag{
+        position: absolute;
+        right: -30rpx;
+        bottom: -6rpx;
+        padding: 6rpx 16rpx;
+        font-size: 22rpx;
+        color: #FAB714;
+        background-color: #FFFFFF;
+        border-radius: 100rpx;
+        box-shadow: $box-shadow2;
+        image{
+          width: 26rpx;
+        }
+      }
+		}
+    .name{
+      color: $themeTextColor;
+      font-weight: 700;
+      margin-bottom: $margin-bottom;
+      padding-top: 28rpx;
+    }
+  }
+}
+</style>
