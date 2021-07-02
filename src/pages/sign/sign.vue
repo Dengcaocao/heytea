7b4334b59ca0308becdc7281b5fcd28b8e3c68f3
dengwei <1536698087@qq.com>
2021-07-02 14:22:30 +0800

    签到排行榜信息

--- src/pages/sign/sign.vue
+++ src/pages/sign/sign.vue
@@ -30,10 +30,19 @@
       </view>
       <!-- 记录 -->
       <view class="record" :style="{height: scorllHeight}">
-        <!-- <u-cell-group>
-          <u-cell-item icon="setting-fill" title="个人设置"></u-cell-item>
-          <u-cell-item icon="integral-fill" title="会员等级" value="新版本"></u-cell-item>
-        </u-cell-group> -->
+        <u-cell-group>
+          <u-cell-item  title="夕阳无限好" label="夕阳无限好" :arrow="false">
+            <!-- left -->
+            <view class="avatar" slot="icon">
+              <image src="@/assets/images/profile/default.png"></image>
+            </view>
+            <!-- right -->
+            <view class="timeAt" slot="right-icon">
+              <view class="integral">{{1012}}<text>签</text></view>
+              <view class="time">{{new Date().toLocaleDateString()}}</view>
+            </view>
+          </u-cell-item>
+        </u-cell-group>
       </view>
     </view>
   </view>
@@ -206,7 +215,32 @@ export default {
     }
   }
   .record{
-    background-color: royalblue;
+    background-color: #FFFFFF;
+    .avatar{
+      width: 80rpx;
+      height: 80rpx;
+      margin-right: 20rpx;
+      image{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
+    }
+    .timeAt{
+      text-align: right;
+      .integral{
+        font-size: 56rpx;
+        color: $themeColor;
+        text{
+          font-size: 28rpx;
+          margin-left: 10rpx;
+        }
+      }
+      .time{
+        color: $themeTextColor;
+        margin-top: 10rpx;
+      }
+    }
   }
 }
 .sign-pregress.active1::after{
